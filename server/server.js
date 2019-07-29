13ef3db9ccade87003635785282d3d3096ea431d
Craig Livings <Craig@nology.io>
2019-07-29 14:53:30 +0100

    Configured port for heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 3000;
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
@@ -17,7 +17,7 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
+http.listen(port, function() {
     console.log('listening on *:3000');
 });
 
